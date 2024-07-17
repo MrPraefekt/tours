ecdde350e5f2e972c25f199ae9a65c6137a941ed
Grabe, Konstantin <konstantin.grabe@interhyp.de>
2024-07-17 15:43:44 +0200

    styling done

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Loading = () => {
-    return (
-        <div>
-            <h2>Loading...</h2>
-        </div>
-    );
+  return <div className="loading"></div>;
 };
 
 export default Loading;
